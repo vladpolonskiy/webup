45b9127632b9a4028fbe36fb66f11dfaf6ee3ca0
vladpolonskiy <opolo93@mail.ru>
2015-11-22 22:20:26 +0200

    top bar

--- html/js/main.js
+++ html/js/main.js
@@ -1,8 +1,8 @@
-(function($) {
+(function() {
 	jQuery(document).ready(function () {
 		jQuery('.logo-toggle').click(function () {
 			jQuery(this).toggleClass('active');
-			jQuery('.side-nav').toggleClass('active');
+			jQuery('.top-bar').toggleClass('active');
 		});
 	});
-})(jQuery);
+})();

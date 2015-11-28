0d999619b1778c83aab29a624f3ac2f81fbc9c41
vladpolonskiy <opolo93@mail.ru>
2015-11-28 17:58:41 +0200

    1st block with bg img

--- wp/wp-content/themes/bootstrap-basic/page-main.php
+++ wp/wp-content/themes/bootstrap-basic/page-main.php
@@ -7,7 +7,14 @@
  */
 ?>
 <?php get_header(); ?> 
-				<div class="col-md-12" id="main-block-1">
-					<h1>Hello</h1>
-				</div>
-<?php get_footer(); ?> 
+					<header class="col-md-12" id="main-block-1">
+				        <div class="header-content">
+				            <div class="header-content-inner">
+				                <h1>Веб разработка - это просто, если с нами</h1>
+				                <hr>
+				                <p>На этом ресурсе собран обучающий материал и все новинки в мире веб разработки. Следи, обучайся, совершенствуйся</p>
+				                <a href="#about" class="btn btn-primary btn-xl page-scroll">Узнать больше</a>
+				            </div>
+				        </div>
+				    </header>
+				    </div>

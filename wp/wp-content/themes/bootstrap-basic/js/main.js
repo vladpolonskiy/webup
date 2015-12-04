2c17590781d7f463968227f40f043c9395aa6b7e
vladpolonskiy <opolo93@mail.ru>
2015-12-04 22:42:13 +0200

    переделал хедер, поправил футер и добавил к виджетам справа черточку

--- wp/wp-content/themes/bootstrap-basic/js/main.js
+++ wp/wp-content/themes/bootstrap-basic/js/main.js
@@ -1,24 +1,4 @@
 jQuery(function () {
-
-	var navbar = window.mainNav;
-
-	jQuery(window).scroll(function (e) {
-		if(jQuery(document).scrollTop() > 50) {
-			jQuery(navbar).removeClass('affix-top');
-			jQuery(navbar).addClass('affix');
-		} else {
-			jQuery(navbar).removeClass('affix');
-			jQuery(navbar).addClass('affix-top');
-		}
-	});
-
-	
-
-	jQuery('.navbar-default .nav > li').click(function() {
-		jQuery('.navbar-default .nav > li').removeClass('current');
-		jQuery(this).addClass('current');
-	});
-
 	//Search toggle
 	jQuery('.search-form').click(function() {
 		if(!jQuery(this).hasClass('clicked')) {

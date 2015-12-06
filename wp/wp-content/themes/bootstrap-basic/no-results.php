d7fc8f5048a0b2cd345f69bd7b0d268cedb7435d
vladpolonskiy <opolo93@mail.ru>
2015-12-06 18:00:59 +0200

    Widget Search, Page Search

--- wp/wp-content/themes/bootstrap-basic/no-results.php
+++ wp/wp-content/themes/bootstrap-basic/no-results.php
@@ -2,23 +2,23 @@
 /**
  * The template part for displaying message that posts cannot be found.
  * 
- * @package bootstrap-basic
+ * @package webup
  */
 ?>
 <section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php _e('Nothing Found', 'bootstrap-basic'); ?></h1>
+		<h1 class="page-title"><?php _e('Ничего не нашли =(', 'webup'); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content row-with-vspace">
 		<?php if (is_home() && current_user_can('publish_posts')) { ?> 
-			<p><?php printf(__('Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'bootstrap-basic'), esc_url(admin_url('post-new.php'))); ?></p>
+			<p><?php printf(__('Готовы опубликовать Ваш первый пост?? <a href="%1$s">НАчните сейчас</a>.', 'webup'), esc_url(admin_url('post-new.php'))); ?></p>
 		<?php } elseif (is_search()) { ?> 
-			<p><?php _e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'bootstrap-basic'); ?></p>
-			<?php echo bootstrapBasicFullPageSearchForm(); ?> 
+			<p><?php _e('Просим прощения, но по Вашему запросу ничего не найдено. Пожалуйста, попробуйте снова, используя другие ключевые слова.', 'webup'); ?></p>
+			<?php echo get_search_form(); ?> 
 		<?php } else { ?> 
-			<p><?php _e('It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'bootstrap-basic'); ?></p>
-			<?php echo bootstrapBasicFullPageSearchForm(); ?> 
+			<p><?php _e('Возможно, мы не можем найти то, что вы ищете. Вероятно, поиск сможет помочь.', 'webup'); ?></p>
+			<?php echo get_search_form(); ?> 
 		<?php } //endif; ?> 
 	</div><!-- .page-content -->
 </section><!-- .no-results -->
